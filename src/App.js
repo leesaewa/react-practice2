a1c575692c27cd907cd6143f06f112cb11caa4dc
leesaewa <saewa0424@gmail.com>
2023-01-29 00:34:56 +0900

    230129 styled-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  // 이 안에 CSS를 작성
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+// 컴포넌트 확장
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const Btn = styled.button`
+  width: 100%;
+  color: white;
+  border-radius: 10px;
+  border: 0;
+  background-color: thistle;
+  margin-bottom: 10px;
+`;
+
+const Inpuut = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: aquamarine;
+  margin-bottom: 10px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
+      <Btn>Login</Btn>
+      <Btn as="a" href="/">
+        Login
+      </Btn>
+
+      <Inpuut />
+      <Inpuut />
+      <Inpuut />
+      <Inpuut />
+      <Inpuut />
+    </Father>
   );
 }
 
