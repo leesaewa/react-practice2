2b32c2b85dd92df2684e4882af8a55941dc3d3a7
leesaewa <saewa0424@gmail.com>
2023-01-29 01:05:11 +0900

    styled-components: animation

--- src/App.js
+++ src/App.js
@@ -1,52 +1,50 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   // 이 안에 CSS를 작성
   display: flex;
   flex-wrap: wrap;
 `;
 
+const rotation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotation} 1s linear infinite;
 
-// 컴포넌트 확장
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
+  span {
+    font-size: 36px;
 
-const Btn = styled.button`
-  width: 100%;
-  color: white;
-  border-radius: 10px;
-  border: 0;
-  background-color: thistle;
-  margin-bottom: 10px;
-`;
-
-const Inpuut = styled.input.attrs({ required: true, minLength: 10 })`
-  background-color: aquamarine;
-  margin-bottom: 10px;
+    &:hover {
+      color: aliceblue;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
-      <Btn>Login</Btn>
-      <Btn as="a" href="/">
-        Login
-      </Btn>
-
-      <Inpuut />
-      <Inpuut />
-      <Inpuut />
-      <Inpuut />
-      <Inpuut />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>hi</span>
+      </Box>
+    </Wrapper>
   );
 }
 
