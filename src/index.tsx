7e0a5a386abfe9bfd2f75269130b9d287b247f9c
leesaewa <saewa0424@gmail.com>
2023-04-09 22:22:15 +0900

    에러나서 다른 버전으로 회귀시킴

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
@@ -7,17 +7,13 @@ import { theme } from "./theme";
 
 const queryClient = new QueryClient();
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  // <React.StrictMode> url은 변하지만 렌더링이 되지 않아서 div로 변환
-  <div>
+ReactDOM.render(
+  <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
     </QueryClientProvider>
-  </div>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
